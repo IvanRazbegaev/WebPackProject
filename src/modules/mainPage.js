64dca8359938a8d25b464eede84a59c4989410ff
Ivan Razbegaev <sinnless13@gmail.com>
2021-10-30 19:01:17 +0300

    fixed name form not being handled with RegExp

--- src/modules/mainPage.js
+++ src/modules/mainPage.js
@@ -1,11 +1,17 @@
 const mainPage = () => {
   const messageForm = document.querySelector('#form2-message')
   const emailForm = document.querySelectorAll('input[type=email]')
-  const telForm = document.querySelectorAll('input[type=tel]')
+  const telForm = document.querySelectorAll('input[type=tel]');
+  const nameForm = document.querySelectorAll('[name=user_name]')
 
 
   messageForm.addEventListener('input', (e) => {
     e.target.value = e.target.value.replace(/[^а-яё\s-]/gi, '')
+  });
+  nameForm.forEach(value => {
+    value.addEventListener('input', (e) => {
+      e.target.value = e.target.value.replace(/[^а-яё\s-]/gi, '')
+    });
   })
   emailForm.forEach(value => {
     value.addEventListener('input', (e) => {
