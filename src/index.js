3cab7020a0b2ccafcc235b86820d1d67960eb8bf
Ivan Razbegaev <sinnless13@gmail.com>
2021-10-31 18:45:15 +0300

    swiper done

--- src/index.js
+++ src/index.js
@@ -3,9 +3,11 @@ import menu from "./modules/menu";
 import modal from "./modules/modal";
 import calculator from "./modules/calculator";
 import mainPage from "./modules/mainPage";
+import sliderSwiper from "./modules/sliderSwiper";
 
 timer('30 november 2021');
 menu();
 modal();
 calculator();
 mainPage();
+sliderSwiper();
