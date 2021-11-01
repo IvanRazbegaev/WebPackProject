6ef320732ac65c6c51a7bf9aa9d99353fcdf35ef
Ivan Razbegaev <sinnless13@gmail.com>
2021-11-02 00:11:05 +0300

    lesson 22 hard done

--- src/modules/menu.js
+++ src/modules/menu.js
@@ -1,18 +1,20 @@
 const menu = function () {
 
-    const menuBtn = document.querySelector('.menu');
     const menu = document.querySelector('menu');
+    let menuWasClicked = false;
 
-    const handleMenu = () => {
-
-        menu.classList.toggle('active-menu');
-    }
-
-    menuBtn.addEventListener('click', handleMenu);
-    menu.addEventListener("click", (e) => {
-        console.log();
-        if(e.target.classList.contains('close-btn') || e.target.closest('ul>li>a')){
-            handleMenu();
+    document.addEventListener("click", (e) => {
+        if(!menuWasClicked){
+            if (e.target.closest('.menu')) {
+                menu.classList.toggle('active-menu');
+                menuWasClicked = true;
+            }
+        } else {
+            if(e.target.classList.contains('close-btn') ||
+                e.target.closest('ul>li>a') || !e.target.closest(".active-menu")){
+                menu.classList.toggle('active-menu');
+                menuWasClicked = false
+            }
         }
     })
 }
--- src/modules/modal.js
+++ src/modules/modal.js
@@ -1,40 +1,17 @@
 const modal = () => {
     const btnsList = document.querySelectorAll('.popup-btn');
     const modal = document.querySelector('.popup');
-    const screenWidth = document.documentElement.offsetWidth;
-    let idInterval;
-    let count = 0;
 
-    const handleModal = () => {
-        if (!modal.style.display){
-            modal.style.transform = 'translateX(100%)';
-            modal.style.display = 'block';
-            if(screenWidth >= 738){
-                modalShowUp();
-            } else {
-                modal.style.transform = 'translateX(0%)';
-            }
-        } else {
-            modal.style.display = '';
-        }
-    };
 
-    const modalShowUp = () => {
-        count += 5
-        idInterval = requestAnimationFrame(modalShowUp);
-        if (count <= 100){
-            modal.style.transform = `translateX(${100 - count}%)`
-        } else {
-            cancelAnimationFrame(idInterval);
+    btnsList.forEach(value => {
+        value.addEventListener('click', () => {
             modal.style.display = 'block'
-            count = 0;
-        }
-    }
-    btnsList.forEach(value => value.addEventListener('click', handleModal));
+        })
+    });
 
     modal.addEventListener('click', (e) => {
         if(!e.target.closest('.popup-content') || e.target.classList.contains("popup-close")){
-            modal.style.display = ''
+            modal.style.display = 'none'
         }
     })
 
